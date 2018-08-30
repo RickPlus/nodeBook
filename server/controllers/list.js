0204190fb5d3bf4a3a20e7faef5ff1d6bcdf85d5
Rick <rick@172-1-1-93.lightspeed.hstntx.sbcglobal.net>
2018-08-30 15:26:44 +0800

    总算调通，调用数据库数据

--- server/controllers/list.js
+++ server/controllers/list.js
@@ -1,27 +1,30 @@
+const { mysql } = require('../qcloud')
 async function get(ctx, next) {
+  let oR = await mysql.select().table('record').where('book_id',1);
   ctx.state.code = 1
-  ctx.state.data = [
-    {
-      content: '测试数据1',
-      date: '2018-08-30',
-      val: -50
-    },
-    {
-      content: '测试数据2',
-      date: '2018-08-30',
-      val: 50
-    },
-    {
-      content: '测试数据3',
-      date: '2018-08-30',
-      val: -20
-    },
-    {
-      content: '测试数据4',
-      date: '2018-08-30',
-      val: -30
-    }
-  ]
+  ctx.state.data = oR
+  // ctx.state.data = [
+  //   {
+  //     content: '测试数据1',
+  //     date: '2018-08-30',
+  //     val: -50
+  //   },
+  //   {
+  //     content: '测试数据2',
+  //     date: '2018-08-30',
+  //     val: 50
+  //   },
+  //   {
+  //     content: '测试数据3',
+  //     date: '2018-08-30',
+  //     val: -20
+  //   },
+  //   {
+  //     content: '测试数据4',
+  //     date: '2018-08-30',
+  //     val: -30
+  //   }
+  // ]
 }
 
 module.exports = {
